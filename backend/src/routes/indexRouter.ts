1fb70a5816b717fed63efb696c0294ce367842fd
Cedric <75742716+cedorikku@users.noreply.github.com>
2026-01-09 00:16:12 +0800

    chore: separate backend

---
+++ backend/src/routes/indexRouter.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from 'express';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default router;
